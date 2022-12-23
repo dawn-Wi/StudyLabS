019f88e4a07bf8d3ebc80f187491e588553f63b7
dawn <wi825@naver.com>
2022-12-23 18:27:08 +0900

    Initial commit

---
+++ app/src/main/java/com/dawn/studylab/ui/theme/Theme.kt
@@ -0,0 +1,57 @@
+package com.dawn.studylab.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    secondary = Teal200,
+    tertiary = Purple700
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500,
+    secondary = Teal200,
+    tertiary = Purple700,
+    primaryContainer = Purple200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun StudyLabSTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    val studyLabShapes = Shapes(
+        extraSmall = RoundedCornerShape(4.dp),
+        small = RoundedCornerShape(8.dp),
+        medium = RoundedCornerShape(8.dp),
+        large = RoundedCornerShape(12.dp),
+        extraLarge = RoundedCornerShape(16.dp),
+    )
+
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = studyLabShapes,
+        content = content
+    )
+}
